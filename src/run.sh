ce8b18448ae493ecc0e03f6a15a648a5047c3ea3
Han <hanz5@andrew.cmu.edu>
2026-04-25 12:43:00 -0400

    temp

--- src/run.sh
+++ src/run.sh
@@ -63,9 +63,25 @@
 #     --seed 42 --freeze_text_encoder --freeze_vae --use_dit --use_t5 --model_type flow --run_name flow_dit_frozen_t5
 
 
+# python3 ./train.py \
+#     --data_path /home/chealisa/Desktop/genAI/stylebooth_subset/metadata.csv \
+#     --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen \
+#     --resolution 512 \
+#     --batch_size 4 \
+#     --lr 1e-5 \
+#     --weight_decay 1e-2 \
+#     --num_epochs 10 \
+#     --grad_accum_steps 1 \
+#     --max_grad_norm 1.0 \
+#     --save_every_steps 10000 \
+#     --sample_every_steps 50 \
+#     --num_sample_images 2 \
+#     --mixed_precision no \
+#     --seed 42 --freeze_text_encoder --freeze_vae --use_dit --model_type decouple --run_name decouple_dit_frozen --text_guidance_scale 1.0
+
 python3 ./train.py \
     --data_path /home/chealisa/Desktop/genAI/stylebooth_subset/metadata.csv \
-    --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen \
+    --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen_t5 \
     --resolution 512 \
     --batch_size 4 \
     --lr 1e-5 \
@@ -77,53 +93,37 @@ python3 ./train.py \
     --sample_every_steps 50 \
     --num_sample_images 2 \
     --mixed_precision no \
-    --seed 42 --freeze_text_encoder --freeze_vae --use_dit --model_type decouple --run_name decouple_dit_frozen --text_guidance_scale 1.0
+    --seed 42 --freeze_text_encoder --freeze_vae --use_dit --use_t5 --model_type decouple --run_name decouple_dit_frozen_t5 --text_guidance_scale 1.0
 
-# python3 ./train.py \
-#     --data_path /home/chealisa/Desktop/genAI/stylebooth_subset/metadata.csv \
-#     --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen_t5 \
-#     --resolution 512 \
-#     --batch_size 4 \
-#     --lr 1e-5 \
-#     --weight_decay 1e-2 \
-#     --num_epochs 10 \
-#     --grad_accum_steps 1 \
-#     --max_grad_norm 1.0 \
-#     --save_every_steps 10000 \
-#     --sample_every_steps 50 \
-#     --num_sample_images 2 \
-#     --mixed_precision no \
-#     --seed 42 --freeze_text_encoder --freeze_vae --use_dit --use_t5 --model_type decouple --run_name decouple_dit_frozen_t5
+python3 ./train.py \
+    --data_path /home/chealisa/Desktop/genAI/stylebooth_subset/metadata.csv \
+    --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen_noise \
+    --resolution 512 \
+    --batch_size 4 \
+    --lr 1e-5 \
+    --weight_decay 1e-2 \
+    --num_epochs 10 \
+    --grad_accum_steps 1 \
+    --max_grad_norm 1.0 \
+    --save_every_steps 10000 \
+    --sample_every_steps 50 \
+    --num_sample_images 2 \
+    --mixed_precision no \
+    --seed 42 --freeze_text_encoder --freeze_vae --use_dit --model_type decouple --run_name decouple_dit_frozen_add_noise --use_noise --text_guidance_scale 1.0
 
-# python3 ./train.py \
-#     --data_path /home/chealisa/Desktop/genAI/stylebooth_subset/metadata.csv \
-#     --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen_noise \
-#     --resolution 512 \
-#     --batch_size 4 \
-#     --lr 1e-5 \
-#     --weight_decay 1e-2 \
-#     --num_epochs 10 \
-#     --grad_accum_steps 1 \
-#     --max_grad_norm 1.0 \
-#     --save_every_steps 10000 \
-#     --sample_every_steps 50 \
-#     --num_sample_images 2 \
-#     --mixed_precision no \
-#     --seed 42 --freeze_text_encoder --freeze_vae --use_dit --model_type decouple --run_name decouple_dit_frozen_add_noise --use_noise
-
-# python3 ./train.py \
-#     --data_path /home/chealisa/Desktop/genAI/stylebooth_subset/metadata.csv \
-#     --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen_advloss \
-#     --resolution 512 \
-#     --batch_size 4 \
-#     --lr 1e-5 \
-#     --weight_decay 1e-2 \
-#     --num_epochs 10 \
-#     --grad_accum_steps 1 \
-#     --max_grad_norm 1.0 \
-#     --save_every_steps 10000 \
-#     --sample_every_steps 50 \
-#     --num_sample_images 2 \
-#     --mixed_precision no \
-#     --seed 42 --freeze_text_encoder --freeze_vae --use_dit --model_type decouple --run_name decouple_dit_frozen_advanced_loss --use_advanced_loss
+python3 ./train.py \
+    --data_path /home/chealisa/Desktop/genAI/stylebooth_subset/metadata.csv \
+    --output_dir /home/chealisa/Desktop/genAI/model/decouple_outputs/hptune_test/frozen_advloss \
+    --resolution 512 \
+    --batch_size 4 \
+    --lr 1e-5 \
+    --weight_decay 1e-2 \
+    --num_epochs 10 \
+    --grad_accum_steps 1 \
+    --max_grad_norm 1.0 \
+    --save_every_steps 10000 \
+    --sample_every_steps 50 \
+    --num_sample_images 2 \
+    --mixed_precision no \
+    --seed 42 --freeze_text_encoder --freeze_vae --use_dit --model_type decouple --run_name decouple_dit_frozen_advanced_loss --use_advanced_loss --text_guidance_scale 1.0
 
